0e414129845ce58226374dc76705a959768586f8
m3a <mahdiyaralavi@outlook.com>
2015-06-26 17:21:18 +0430

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,4 +10,21 @@ $(document).ready(function(){
 		$(".sign-in-modal").fadeOut(400);
 		$(".opaque-layer").fadeOut(800);
 	});
+	$(".search-txt-box").click(function(){
+		$(this).hide(200);
+		$(".search-input-box").show(200);
+	});
+	$(".search-input-close>div:nth-child(2)").click(function(){
+		$(".search-input-box").hide(200);
+		$(".search-txt-box").show(200);
+	});
+	$(".likes-box").click(function(){
+		if($(".likes-quantity").hasClass("red"))
+			x = -1;
+		else
+			x = 1;
+		$(".likes-quantity").toggleClass("red");
+		$(".likes-quantity").text(parseInt($(".likes-quantity").text()) + x);
+		$(".like-sign").toggleClass("heart-active");
+	});
 });
